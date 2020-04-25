4ece55a4056d99b6f10e7a0654674c3d9b0bd754
davidlewallen <lewallen.david@gmail.com>
2020-04-24 19:24:42 -0600

    feat(routes): adds verify-email route

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import { CreateAccount } from "./create-account";
+import { VerifyEmail } from "./verify-email";
 
 export const Routes = () => (
   <>
@@ -10,6 +11,10 @@ export const Routes = () => (
     </Route>
     <Route path="/create-account">
       <CreateAccount />
+    </Route>
+
+    <Route path="/verify-email">
+      <VerifyEmail />
     </Route>
 
     <Route path="/login">Login Route</Route>
